e25ffb1e3390f19d64f1f0822724498228736145
Gerardo Belot <gbelot2003@hotmail.com>
2024-01-08 02:16:33 -0600

    🚧 La  relacion de cuenta usuario recibe mas de 2 parametros

--- tests/Feature/AccountsTest.php
+++ tests/Feature/AccountsTest.php
@@ -21,18 +21,63 @@ class AccountsTest extends TestCase
         $MainUser = new User();
         $testUser = $MainUser->factory()->createOne();
 
-
-
         // Crear Cuenta
         // La cuenta tiene relacion muchos a muchos
         $te = new Account();
-
         $account = $te->factory()->createOne();
-        $account->users()->sync($testUser);
+
+
+        $data = [
+            1 => [
+                'user_id' => $testUser->id,
+                'account_id' => $account->id,
+                'type_user' => 1,
+            ]
+        ];
+
+        $account->users()->sync($data);
 
         $this->assertDatabaseHas('accounts', [
             'document_number' => $account->document_number,
             'name' => $account->name
+        ]);
+    }
+
+    /** @test */
+    public function an_account_can_have_more_than_one_user()
+    {
+        // Creamos a un usuario para relacionar con
+        // la cuenta
+        $MainUser = new User();
+        $testUser = $MainUser->factory()->createOne();
+
+        $secundUser = new User();
+        $test2User = $secundUser->factory()->createOne();
+
+        // Crear Cuenta
+        // La cuenta tiene relacion muchos a muchos
+        $te = new Account();
+        $account = $te->factory()->createOne();
+
+        $data = [
+            1 => [
+                'user_id' => $testUser->id,
+                'account_id' => $account->id,
+                'type_user' => 1,
+            ],
+            2 => [
+                'user_id' => $test2User->id,
+                'account_id' => $account->id,
+                'type_user' => 2,
+            ]
+        ];
+
+        $account->users()->sync($data);
+
+        $this->assertDatabaseHas('account_user', [
+            'account_id' => $account->id,
+            'user_id' => $test2User->id,
+            'type_user' => 2,
         ]);
     }
 }
