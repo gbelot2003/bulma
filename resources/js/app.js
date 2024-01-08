aeca65483d4bc626c674444a5bafbc2619d4707f
Gerardo Belot <gbelot2003@hotmail.com>
2024-01-07 23:00:21 -0600

    🚧 configurando plantilla

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,8 @@
 import './bootstrap';
-
-import './datatables-simple-demo';
 import './scripts';
+import './demo/chart-area-demo';
+import './demo/chart-bar-demo';
+import './demo/chart-pie-demo';
+import './demo/datatables-demo';
+import 'simple-datatables';
+import './datatables-simple-demo';
