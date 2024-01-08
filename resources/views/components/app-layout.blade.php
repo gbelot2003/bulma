9216ec241d4d6cad86e64cb9c43410498967fff7
Gerardo Belot <gbelot2003@hotmail.com>
2024-01-07 23:54:36 -0600

    🚧 actualizando boton de logout

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>{{ $title . " | " . env('APP_NAME') ?? 'Funda Of Web IT' }}</title>
+    <title>{{ $title . ' | ' . env('APP_NAME') ?? 'Funda Of Web IT' }}</title>
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     <!-- Scripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js"
+        crossorigin="anonymous"></script>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
 </head>
@@ -18,17 +20,17 @@
 <body class="sb-nav-fixed">
     <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
         <!-- Navbar Brand-->
-        <a class="navbar-brand ps-3" href="index.html">Start Bootstrap</a>
+        <a class="navbar-brand ps-3" href="/admin">{{ env('APP_NAME') }}</a>
         <!-- Sidebar Toggle-->
         <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i
                 class="fas fa-bars"></i></button>
         <!-- Navbar Search-->
         <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
             <div class="input-group">
-                <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..."
+                {{-- <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..."
                     aria-describedby="btnNavbarSearch" />
                 <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i
-                        class="fas fa-search"></i></button>
+                        class="fas fa-search"></i></button> --}}
             </div>
         </form>
         <!-- Navbar-->
@@ -42,7 +44,13 @@
                     <li>
                         <hr class="dropdown-divider" />
                     </li>
-                    <li><a class="dropdown-item" href="#!">Logout</a></li>
+                    <li><a class="dropdown-item" class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+                         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </li>
                 </ul>
             </li>
         </ul>
@@ -131,14 +139,14 @@
         <div id="layoutSidenav_content">
             <main>
                 <div class="container-fluid px-4">
-                    <h1 class="mt-4">{{  $title ?? 'Funda Of Web IT' }}</h1>
+                    <h1 class="mt-4">{{ $title ?? 'Funda Of Web IT' }}</h1>
                     <x-layouts.breadcrumb>
                         <li class="breadcrumb-item active">Dashboard</li>
-                    </x-layout.breadcrumb>
+                        </x-layout.breadcrumb>
 
-                    <div class="row">
-                        {{ $slot }}
-                        {{-- <div class="col-xl-12 col-md-12">
+                        <div class="row">
+                            {{ $slot }}
+                            {{-- <div class="col-xl-12 col-md-12">
                             <div class="card bg-primary text-white mb-4">
                                 <div class="card-body">Primary Card</div>
                                 <div class="card-footer d-flex align-items-center justify-content-between">
@@ -148,7 +156,7 @@
                             </div>
                         </div> --}}
 
-                        {{-- <div class="col-xl-3 col-md-6">
+                            {{-- <div class="col-xl-3 col-md-6">
                             <div class="card bg-warning text-white mb-4">
                                 <div class="card-body">Warning Card</div>
                                 <div class="card-footer d-flex align-items-center justify-content-between">
@@ -178,11 +186,11 @@
                             </div>
                         </div> --}}
 
-                    </div>
+                        </div>
 
-                    <div class="row">
-                        <!-- Charts -->
-                        {{-- <div class="col-xl-6">
+                        <div class="row">
+                            <!-- Charts -->
+                            {{-- <div class="col-xl-6">
                             <div class="card mb-4">
                                 <div class="card-header">
                                     <i class="fas fa-chart-area me-1"></i>
@@ -202,9 +210,9 @@
                                         height="40"></canvas></div>
                             </div>
                         </div> --}}
-                        <!-- Charts -->
-                    </div>
-                    {{-- <div class="card mb-4">
+                            <!-- Charts -->
+                        </div>
+                        {{-- <div class="card mb-4">
                         <div class="card-header">
                             <i class="fas fa-table me-1"></i>
                             DataTable Example
